b9011400b504725f4e4ec7061e7ef3f60703181a
Ramillies-50019 <ian.sticklen@btinternet.com>
2018-05-10 17:09:55 +0100

    as previous but with files actually saved

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define('user', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    timestamps: false,
+    tableName: 'user',
+    freezeTableName: true
+  })
